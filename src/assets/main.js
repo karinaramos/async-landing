cc13e7c8009d2b13620cd5340253300f0fc2df41
karinaramos <kramosq@gmail.com>
2024-06-11 12:29:54 -0500

    added images

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,14 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC74znRN81-m7V-RdZqqn7tg&part=snippet%2Cid&order=date&maxResults=9';
+/*const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC74znRN81-m7V-RdZqqn7tg&part=snippet%2Cid&order=date&maxResults=9';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '0d0f55b37fmshcba70fab426435bp19efcbjsnf284a75d93da',
+		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+  }
+};*/
+    
+const API = 'https://star-wars-characters.p.rapidapi.com/46DYBV/star_wars_characters';
 
 const content = null || document.getElementById('content');
 
@@ -6,7 +16,7 @@ const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '0d0f55b37fmshcba70fab426435bp19efcbjsnf284a75d93da',
-		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+		'x-rapidapi-host': 'star-wars-characters.p.rapidapi.com'
 	}
 };
 
@@ -18,22 +28,24 @@ async function fetchData(urlApi) {
 
 (async () => {
   try {
-    const videos = await fetchData(API);
+    const persons = await fetchData(API);
     let view = `
-    ${videos.items?.map(video => `
-        <div class="group relative">
+    ${persons.map(person => `
+        <div class="group relative rounded-md">
           <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1  overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="/src/assets/images/${person.id}.jpg" alt="${person.name}" class="w-full">
           </div>
-          <div class="mt-4 flex justify-between">
-            <h3 class="text-sm text-gray-700">
+          <div class="mt-0 flex flex-col justify-between bg-white p-3">
+            <h3 class="text-lg text-black">
               <span aria-hidden="true" class="absolute inset-0"></span>
-              ${video.snippet.title}
+              <strong>${person.name}</strong>
             </h3>
+            <p class="text-sm text-gray-700"><strong>Planeta Natal:</strong> ${person.homeworld}</p>
+            <p class="text-sm text-gray-700"><strong>Fecha de Nacimiento:</strong> ${person.birth_year}</p>
           </div>
         </div>
-      `).slice(0,8).join('')}
+      `).slice(0,12).join('')}
     `;
     content.innerHTML = view;
   } catch (error) {
